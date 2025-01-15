514106aa540c2cba51fbc56ef433d083e9566460
EC2 Default User <ec2-user@ip-172-31-88-65.ec2.internal>
2025-01-15 07:44:37 +0000

    task-1

---
+++ main.tf
@@ -0,0 +1,39 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "52297389-bucket"
+    key    = "terraform-task-1.tfstate"
+    dynamodb_table = "lock-state"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+resource "aws_instance" "example_server" {
+  ami           = "ami-0e2c8caa4b6378d8c"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "instance1"
+  }
+}
+
+resource "aws_s3_bucket" "s3_bucket" {
+  bucket = "dbs-training-bucket"
+
+  tags = {
+     Terraform   = "true"
+    Environment = "dev"
+  }
+}
